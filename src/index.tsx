630b8805461c93c4b2cd7de7bef9052eddc23db3
Matt Rochford <mattjohnrochford@gmail.com>
2020-03-03 21:40:38 -0800

    load auth on init

--- src/index.tsx
+++ src/index.tsx
@@ -4,36 +4,40 @@ import HoundsWeek from "./app/HoundsWeek";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 
+// eslint-disable-next-line
 import * as api from "@happyhoundhotel/hounds-ts";
+// eslint-disable-next-line
+import {HoundsSettings} from "./util/settings/SettingsProvider";
+import {WebSettings} from "./util/settings/WebSettingsProvider";
 
 export const ApiConfig = React.createContext({} as api.IHoundsConfig);
 
-const API_CONFIG: api.IHoundsConfig = {
-    apiURL: "https://hhh-scheduler-testing.herokuapp.com",
-    apiVersion: "0.3.4",
-    apiAuth: {
-        username: "",
-        token: "",
-    },
-};
+const settingsProvider = new WebSettings();
+const settings = settingsProvider.load();
 
-api.login(
-    "matt",
-    "",
-    API_CONFIG.apiURL,
-)
-    .then((auth: api.IHoundAuth) => {
-        const apiConfig: api.IHoundsConfig = {
-            ...API_CONFIG,
-            apiAuth: auth,
-        };
+// Load settings from a provider
+
+renderWeek({
+    apiURL: settings.apiUrl,
+    apiVersion: "0.3.4",
+    apiAuth: settings.auth,
+});
+
+// eslint-disable-next-line
+function renderWeek(apiConfig: api.IHoundsConfig) {
+    api.checkAuthentication(apiConfig).then((success) => {
+        if (!success) {
+            alert("Authentication failed");
+            return;
+        }
+
         ReactDOM.render(
             <ApiConfig.Provider value={apiConfig}>
                 <HoundsWeek />
             </ApiConfig.Provider>,
-            document.getElementById("root"),
-        );
+            document.getElementById("root"));
     });
+}
 
 
 // If you want your app to work offline and load faster, you can change

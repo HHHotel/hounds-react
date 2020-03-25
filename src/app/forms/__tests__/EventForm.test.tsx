1acb569708e83ca034f9c3c569dc9a3695f17ef5
Matt Rochford <mattjohnrochford@gmail.com>
2020-03-24 20:22:30 -0700

    describe and it for tests

--- src/app/forms/__tests__/EventForm.test.tsx
+++ src/app/forms/__tests__/EventForm.test.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import EventForm from "../EventForm";
-import { render, fireEvent, cleanup, screen } from "@testing-library/react";
-// eslint-disable-next-line no-unused-var
+import {
+    render,
+    fireEvent,
+    cleanup,
+    screen,
+    RenderResult
+} from "@testing-library/react";
+// eslint-disable-next-line
 import * as api from "@happyhoundhotel/hounds-ts";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
@@ -9,77 +15,63 @@ import DateFnsUtils from "@date-io/date-fns";
 
 const theme = createMuiTheme();
 
-test("Loads and displays form", () => {
-    const click = jest.fn();
-    const element = <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <ThemeProvider theme={theme}>
-            <EventForm onSubmit={click} />
-        </ThemeProvider>
-    </MuiPickersUtilsProvider>;
-
-    const { queryByText, queryByLabelText, queryByTestId } = render(element);
-
-    const submitButton = queryByText(/submit/i);
-    expect(submitButton).toBeInTheDocument();
-    const textInput = queryByLabelText(/event text/i);
-    expect(textInput).toBeInTheDocument();
-    const startInput = queryByTestId("start-date");
-    expect(startInput).toBeInTheDocument();
-    const endInput = queryByTestId("end-date");
-    expect(endInput).toBeInTheDocument();
-    const typeInput = queryByLabelText(/type/i);
-    expect(typeInput).toBeInTheDocument();
-
-    submitButton && fireEvent.click(submitButton);
-    expect(click).toBeCalled();
-});
-
-/*
-test("Submits correct event", () => {
-    const click = jest.fn();
-    const element = <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <ThemeProvider theme={theme}>
-            <EventForm onSubmit={click} />
-        </ThemeProvider>
-    </MuiPickersUtilsProvider>;
-
-    const {
-        debug,
-        queryByText,
-        queryByLabelText,
-    } = render(element);
-    debug();
-
-    const submitButton = queryByText(/submit/i);
-    const textInput = queryByLabelText(/event text/i);
-    textInput && fireEvent.change(textInput, { target: { value: "Testing" } });
-    // const startInput = queryByTestId("start-date");
-    const startInput = document.querySelector("input[name='startDate']");
-    // console.log(startInput);
-    startInput && fireEvent.change(
-        startInput,
-        new Date("10-10-1919 8:00"),
+describe("New event form", () => {
+    let wrapper: RenderResult;
+    const submitMock = jest.fn();
+    const element = ( // TODO Wrap becomes a shared util
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <ThemeProvider theme={theme}>
+                <EventForm onSubmit={submitMock} />
+            </ThemeProvider>
+        </MuiPickersUtilsProvider>
     );
-    // const endInput = queryByTestId("end-date");
-    const endInput = document.querySelector("input[name='endDate']");
-    endInput && fireEvent.change(
-        endInput,
-        new Date("10-10-1919 16:00"),
-    );
-    const typeInput = queryByLabelText(/event type/i);
-    typeInput && fireEvent.click(typeInput);
-    const choice = queryByText(/general/i);
-    choice && fireEvent.click(choice);
 
-    submitButton && fireEvent.click(submitButton);
-    expect(click).toHaveBeenCalledWith({
-        text: "Testing",
-        type: "general",
-        startDate: new Date("10-10-1919 8:00"),
-        endDate: new Date("10-10-1919 16:00"),
-        id: "",
+    beforeEach(() => {
+        wrapper = render(element);
     });
+
+    it("Has a submit button", () => {
+        const submitButton = wrapper.queryByText(/submit/i);
+        expect(submitButton).toBeInTheDocument();
+    });
+
+    it("Has an event text input", () => {
+        const textInput = wrapper.queryByLabelText(/event text/i);
+        expect(textInput).toBeInTheDocument();
+    });
+
+    it("Changes event text input", () => {
+        const textInput = wrapper.queryByLabelText(/event text/i);
+        textInput && fireEvent;
+        expect(textInput).toBeInTheDocument();
+    });
+
+    it("Has an event type input", () => {
+        const typeInput = wrapper.queryByLabelText(/type/i);
+        expect(typeInput).toBeInTheDocument();
+    });
+
+    it("Has a start input", () => {
+        const startInput = wrapper.queryByTestId("start-date");
+        expect(startInput).toBeInTheDocument();
+    });
+
+    it("Has an end input", () => {
+        const endInput = wrapper.queryByTestId("end-date");
+        expect(endInput).toBeInTheDocument();
+    });
+
+    it("Submits on button click", () => {
+        const button = wrapper.queryByText(/submit/i);
+        button && fireEvent.click(button);
+        expect(submitMock).toBeCalled();
+    });
+    // expect(submitMock).toBeCalledWith({
+    //     text: "testing",
+    //     type: "general",
+    //     startDate: undefined,
+    //     endDate: undefined
+    // });
 });
-*/
 
 afterEach(cleanup);

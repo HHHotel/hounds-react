85da0de2b6607db6595286d2604cdc05162ec224
Matt Rochford <mattjohnrochford@gmail.com>
2020-07-29 12:04:01 -0700

    move provider wrappers to index

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,9 +5,6 @@ import {
     Switch,
     Route,
 } from "react-router-dom";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import DateFnsUtils from "@date-io/date-fns";
 
 import "./App.css";
 
@@ -30,9 +27,6 @@ import HoundsWeek from "./routes/main/HoundsWeek";
 import HoundsSettingsPage from "./routes/HoundsSettings";
 import DogProfile from "./routes/profile/Profile";
 import { getWeekArray } from "./routes/main/utils";
-import Form from "./forms/Form";
-
-const theme = createMuiTheme();
 
 /**
  * Entrypoint for the application
@@ -62,84 +56,41 @@ export default function App() {
     }, []);
 
     return (
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Router>
-                <ThemeProvider theme={theme}>
-                    <SettingsContext.Provider
-                        value={{
-                            settings,
-                            setSettings: saveSettings,
-                        }}
-                    >
-                        <ApiConfigContext.Provider
-                            value={{ apiConfig: apiConf, setAuth }}
-                        >
-                            <Switch>
-                                <PrivateRoute path="/app/main">
-                                    <HoundsWeek
-                                        dates={dates}
-                                        setDates={setDates}
-                                    />
-                                </PrivateRoute>
-                                <Route path="/app/settings">
-                                    <HoundsSettingsPage />
-                                </Route>
-                                <Route path="/app/profile/:dogId">
-                                    <DogProfile />
-                                </Route>
-                                <Route path="/login">
-                                    {apiConf.apiAuth ? (
-                                        <Redirect to={"/app/main"} />
-                                    ) : (
-                                        <HoundsLogin />
-                                    )}
-                                </Route>
-                                <Route path="/test">
-                                    <Form
-                                        schema={{
-                                            data: new Map([
-                                                ["dogName", "string"],
-                                                ["clientName", "string"],
-                                            ]),
-                                        }}
-                                        onSubmit={console.log}
-                                    ></Form>
-                                </Route>
-                                <Route path="*">
-                                    <Redirect to="/app/main" />
-                                </Route>
-                            </Switch>
-                        </ApiConfigContext.Provider>
-                    </SettingsContext.Provider>
-                </ThemeProvider>
-            </Router>
-        </MuiPickersUtilsProvider>
-    );
-}
-
-/**
- * A wrapper for <Route> that redirects to the login
- * screen if you're not yet authenticated.
- * @param {any} props properties
- * @return {React.ReactElement} el
- */
-function PrivateRoute({ children, ...rest }: any) {
-    const { apiConfig } = React.useContext(ApiConfigContext);
-    return (
-        <Route
-            {...rest}
-            render={({ location }: any) =>
-                apiConfig.apiAuth ? (
-                    children
-                ) : (
-                    <Redirect
-                        to={{
-                            pathname: "/login",
-                            state: { from: location },
-                        }}
-                    />
-                )
-            }
-        />
+        <SettingsContext.Provider
+            value={{
+                settings,
+                setSettings: saveSettings,
+            }}
+        >
+            <ApiConfigContext.Provider value={{ apiConfig: apiConf, setAuth }}>
+                <Router>
+                    <Switch>
+                        <Route path="/app/main">
+                            {apiConf.apiAuth ? (
+                                <HoundsWeek dates={dates} setDates={setDates} />
+                            ) : (
+                                <Redirect to={"/login"} />
+                            )}
+                        </Route>
+                        <Route path="/login">
+                            {apiConf.apiAuth ? (
+                                <Redirect to={"/app/main"} />
+                            ) : (
+                                <HoundsLogin />
+                            )}
+                        </Route>
+                        <Route path="/app/settings">
+                            <HoundsSettingsPage />
+                        </Route>
+                        <Route path="/app/profile/:dogId">
+                            <DogProfile />
+                        </Route>
+                        <Route path="*">
+                            <Redirect to="/app/main" />
+                        </Route>
+                    </Switch>
+                </Router>
+            </ApiConfigContext.Provider>
+        </SettingsContext.Provider>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./app/App";
-render(<App />, document.getElementById("root"));
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+
+const theme = createMuiTheme();
+
+render(
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </MuiPickersUtilsProvider>,
+    document.getElementById("root")
+);
